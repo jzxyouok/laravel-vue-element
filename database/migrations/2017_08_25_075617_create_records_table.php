1826dbe66db76dc2a617ade61a033694a3b6f944
linlianmin <linlianmin@eoffice.com>
2017-09-05 16:20:07 +0800

    创建migration文件，代码规范

--- database/migrations/2017_08_25_075617_create_records_table.php
+++ database/migrations/2017_08_25_075617_create_records_table.php
@@ -16,6 +16,11 @@ class CreateRecordsTable extends Migration
         Schema::create('records', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
+            $table->integer('user_id')->comment('用户id');
+            $table->string('action')->default('')->comment('控制器/方法名');
+            $table->text('params')->default('')->comment('参数');
+            $table->string('text')->default('')->comment('说明');
+            $table->tinyInteger('status')->default(1)->comment('状态(成功|失败)');
             $table->timestamps();
         });
     }
